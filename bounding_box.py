8ba8486f1fb2ad1863d95662c4f1c07b00be6b1b
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-07-04 16:49:05 +0530

    chore: Refactor draw_bounding_box function to handle incomplete and invalid annotations

--- bounding_box.py
+++ bounding_box.py
@@ -10,7 +10,7 @@ annotation_paths = data["train_annotations"]
 class_names = data["names"]
 output_dir = "bb_images/"
 
-def draw_bounding_box(image_path,annotation_path,output_path):
+def draw_bounding_box(image_path, annotation_path, output_path):
     image = cv2.imread(image_path)
     h, w, _ = image.shape
     with open(annotation_path, 'r') as f:
@@ -18,10 +18,26 @@ def draw_bounding_box(image_path,annotation_path,output_path):
 
     for ann in annotations:
         ann = ann.strip().split()
-        class_id = int(ann[0])
-        if class_id < 0 or class_id >= 14:
+        
+        if len(ann) == 0:
+            print(f"Empty annotation found: {ann}")
+            continue
+
+        if len(ann) < 5:
+            print(f"Incomplete annotation found: {ann}")
+            continue
+        
+        try:
+            class_id = int(ann[0])
+            
+        except ValueError:
+            print(f"Non-integer class_id found: {ann[0]}")
+            continue
+
+        if class_id < 0 or class_id >= len(class_names):
             print(f"Invalid class_id {class_id} in file {annotation_path}")
             continue
+        
         x_center = float(ann[1]) * w
         y_center = float(ann[2]) * h
         bbox_width = float(ann[3]) * w
@@ -31,7 +47,8 @@ def draw_bounding_box(image_path,annotation_path,output_path):
         x2 = int(x_center + bbox_width / 2)
         y2 = int(y_center + bbox_height / 2)
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(image, class_names[class_id], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        cv2.putText(image, class_names[class_id], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+    
     cv2.imwrite(output_path, image)
 
 for image_dir, annotation_dir in zip(image_paths, annotation_paths):
